7ce5d3afe48c8b201f937b6e88e2f86c3d4cb4b0
Yasura Laksitha <157275383+YasuraLaksitha@users.noreply.github.com>
2025-07-16 19:49:12 +0530

    Merge pull request #4
    * Enable redis caching with the TTL value of 15 minutes; wrap the Weath…

--- src/main/java/com/skycast/weatherservice/WeatherserviceApplication.java
+++ src/main/java/com/skycast/weatherservice/WeatherserviceApplication.java
@@ -2,8 +2,10 @@ package com.skycast.weatherservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WeatherserviceApplication {
 
 	public static void main(String[] args) {
